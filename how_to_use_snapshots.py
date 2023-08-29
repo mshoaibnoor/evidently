8226591f4fde631474021bd9ef3da36372d5e254
mshoaibnoor <muhammad.shoaib.masood@gmail.com>
2023-08-29 10:12:31 +0500

    update report content

--- how_to_use_snapshots.py
+++ how_to_use_snapshots.py
@@ -13,15 +13,39 @@ bcancer = bcancer_data.frame
 bcancer_ref = bcancer.sample(n=300, replace=False)
 bcancer_cur = bcancer.sample(n=200, replace=False)
 
-
+# data drift report
 data_drift_report = Report(
-    metrics=[DataDriftPreset(),
-             DataQualityPreset(),
-             TargetDriftPreset()
-             ]
+    metrics=[
+        DataDriftPreset()
+        ]
 )
 
 data_drift_report.run(reference_data=bcancer_ref, current_data=bcancer_cur)
 
 
 data_drift_report.save_html('/scikit_learn_data/workspace/data_drift_report.html')
+
+# data drift report
+
+# data quality report
+data_quality_report = Report(
+    metrics=[
+        DataQualityPreset()
+    ]
+)
+data_quality_report.run(reference_data=bcancer_ref, current_data=bcancer_cur)
+data_quality_report.save_html('/scikit_learn_data/workspace/data_quality_report.html')
+
+# data quality report
+
+
+# target drift report
+target_drift_report = Report(
+    metrics=[
+        TargetDriftPreset()
+    ]
+)
+target_drift_report.run(reference_data=bcancer_ref, current_data=bcancer_cur)
+target_drift_report.save_html('/scikit_learn_data/workspace/target_drift_report.html')
+
+# target drift report
