6fafddb27a9d8a4b0757dca321e1420a9bf173a4
mshoaibnoor <muhammad.shoaib.masood@gmail.com>
2023-08-29 10:28:00 +0500

    update add new reports

--- how_to_use_snapshots.py
+++ how_to_use_snapshots.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn import datasets
 
 from evidently.report import Report
-from evidently.metric_preset import DataDriftPreset,DataQualityPreset,TargetDriftPreset
+from evidently.metric_preset import DataDriftPreset,DataQualityPreset,TargetDriftPreset, RegressionPreset, ClassificationPreset
 
 
 bcancer_data = datasets.load_breast_cancer(as_frame=True)
@@ -49,3 +49,24 @@ target_drift_report.run(reference_data=bcancer_ref, current_data=bcancer_cur)
 target_drift_report.save_html('/scikit_learn_data/workspace/target_drift_report.html')
 
 # target drift report
+
+# regression performance report
+regression_performance_report = Report(
+    metrics=[
+        RegressionPreset()
+    ]
+)
+regression_performance_report.run(reference_data=bcancer_ref, current_data=bcancer_cur)
+regression_performance_report.save_html('/scikit_learn_data/workspace/regresion_performance_report.html')
+# regression performance report
+
+# classification performance report
+classification_performance_report = Report(
+    metrics=[
+        ClassificationPreset(probas_threshold=0.7)
+    ]
+)
+
+classification_performance_report.run(reference_data=bcancer_ref, current_data=bcancer_cur)
+classification_performance_report.save_html('/scikit_learn_data/workspace/classification_performance_report.html')
+# classification performance report
