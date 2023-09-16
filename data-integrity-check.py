dccab820bac0c3d12efaeb06b38bfd992301defe
mshoaibnoor <muhammad.shoaib.masood@gmail.com>
2023-09-16 10:58:17 +0500

    update

--- data-integrity-check.py
+++ data-integrity-check.py
@@ -1,3 +1,4 @@
+#pandas 2.0.1 is required to run deepchecks successfully
 from deepchecks.tabular.suites import data_integrity
 from deepchecks.tabular import Dataset
 import pandas as pd
@@ -5,9 +6,9 @@ import os
 
 def data_integrity_check():
     data_df = pd.read_csv('/app/data/adult-education-dataset.csv')
-    # dataset = Dataset(data_df, label='class', cat_features=[])
-    # suite_result = data_integrity().run(dataset)
-    # suite_result.save_as_html('data_validation.html')
+    dataset = Dataset(data_df, label='class', cat_features=[])
+    suite_result = data_integrity().run(dataset)
+    suite_result.save_as_html('data_validation.html')
 
 
 if __name__ == "__main__":
@@ -16,5 +17,4 @@ if __name__ == "__main__":
     os.chdir('/dataintegritycheck/')
     cwd = os.getcwd()
     print(cwd)
-    os.environ['MPLCONFIGDIR']='/dataintegritycheck/.config/matplotlib'
     data_integrity_check()
