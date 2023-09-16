e8d7fa6dad868b910ba72d02cb98642ce9cd4293
mshoaibnoor <muhammad.shoaib.masood@gmail.com>
2023-09-16 11:04:09 +0500

    update

--- data-integrity-check.py
+++ data-integrity-check.py
@@ -5,7 +5,7 @@ import pandas as pd
 import os
 
 def data_integrity_check():
-    data_df = pd.read_csv('/app/data/adult-education-dataset.csv')
+    data_df = pd.read_csv('/dataintegritycheck/data/adult-education-dataset.csv')
     dataset = Dataset(data_df, label='class', cat_features=[])
     suite_result = data_integrity().run(dataset)
     suite_result.save_as_html('data_validation.html')
