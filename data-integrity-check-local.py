834884f29ed96298b0a90f7a90a671f368c0b1f7
mshoaibnoor <muhammad.shoaib.masood@gmail.com>
2023-09-16 17:59:59 +0500

    add class imbalance check

--- data-integrity-check-local.py
+++ data-integrity-check-local.py
@@ -1,9 +1,15 @@
 #pandas 2.0.1 is required to run deepchecks successfully
 from deepchecks.tabular.suites import data_integrity
 from deepchecks.tabular import Dataset
+from deepchecks.tabular.checks import ClassImbalance
 import pandas as pd
 import os
 from datetime import datetime
+
+def show_data():
+    data_df = pd.read_csv('app/data/adult-education-dataset.csv')
+    dataset = Dataset(data_df, label='class', cat_features=[])
+    print(dataset)
 
 def data_integrity_check():
     data_df = pd.read_csv('app/data/adult-education-dataset.csv')
@@ -12,9 +18,19 @@ def data_integrity_check():
     dt = datetime.now()
     dt = str(dt)
     dt = dt.replace(' ','-')
-    file_name = 'app/output/dataintegritycheck/data_validation-' + dt + '.html'
-    print(file_name)
+    file_name = 'app/output/dataintegritycheck/data_integrity-' + dt + '.html'
     suite_result.save_as_html(file_name)
+
+def class_imbalance():
+    data_df = pd.read_csv('app/data/adult-education-dataset.csv')
+    dataset = Dataset(data_df, label='class', cat_features=[])
+    # ci = ClassImbalance().run(dataset)
+    ci = ClassImbalance().add_condition_class_ratio_less_than(0.10).run(dataset)
+    dt = datetime.now()
+    dt = str(dt)
+    dt = dt.replace(' ','-')
+    file_name = 'app/output/dataintegritycheck/class-imbalance-' + dt + '.html'
+    ci.save_as_html(file_name)
 
 
 if __name__ == "__main__":
@@ -24,3 +40,5 @@ if __name__ == "__main__":
     # cwd = os.getcwd()
     # print(cwd)
     data_integrity_check()
+    # show_data()
+    class_imbalance()
--- data-integrity-check.py
+++ data-integrity-check.py
@@ -1,14 +1,26 @@
 #pandas 2.0.1 is required to run deepchecks successfully
 from deepchecks.tabular.suites import data_integrity
 from deepchecks.tabular import Dataset
+from deepchecks.tabular.checks import ClassImbalance
 import pandas as pd
 import os
+from datetime import datetime
 
 def data_integrity_check():
     data_df = pd.read_csv('/app/data/adult-education-dataset.csv')
     dataset = Dataset(data_df, label='class', cat_features=[])
     suite_result = data_integrity().run(dataset)
     suite_result.save_as_html('/app/output/dataintegritycheck/data_validation.html')
+
+def class_imbalance():
+    data_df = pd.read_csv('app/data/adult-education-dataset.csv')
+    dataset = Dataset(data_df, label='class', cat_features=[])
+    ci = ClassImbalance().add_condition_class_ratio_less_than(0.10).run(dataset)
+    dt = datetime.now()
+    dt = str(dt)
+    dt = dt.replace(' ','-')
+    file_name = 'app/output/dataintegritycheck/class-imbalance-' + dt + '.html'
+    ci.save_as_html(file_name)
 
 
 if __name__ == "__main__":
@@ -18,3 +30,4 @@ if __name__ == "__main__":
     cwd = os.getcwd()
     print(cwd)
     data_integrity_check()
+    class_imbalance()
